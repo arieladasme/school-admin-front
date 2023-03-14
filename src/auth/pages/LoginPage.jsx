02c61e344e4fe94dd67692186a458a086897c610
ariel <ariel.wm1@gmail.com>
2023-03-14 13:21:02 -0300

    adding form & dispatch

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,13 +1,25 @@
+import { useDispatch } from 'react-redux'
 import { Grid, Typography, TextField, Alert, Button, Link } from '@mui/material'
 import { Google } from '@mui/icons-material'
 import { Link as RouterLink } from 'react-router-dom'
 import { useForm } from '../../hooks'
+import { checkAuth, startGoogleSignIn } from '../../store/auth'
 
 export const LoginPage = () => {
+  const dispatch = useDispatch()
   const { email, password, onInputChange } = useForm({
     email: '',
     password: '',
   })
+
+  const onSubmit = e => {
+    e.preventDefault()
+    dispatch(checkAuth(email, password))
+  }
+
+  const onGoogleSignIn = () => {
+    dispatch(startGoogleSignIn())
+  }
 
   return (
     <Grid
@@ -27,53 +39,55 @@ export const LoginPage = () => {
         <Typography variant="h5" sx={{ mb: 1 }}>
           Login
         </Typography>
-        <Grid container>
-          <Grid item xs={12} sx={{ mt: 2 }}>
-            <TextField
-              label="Correo"
-              type="email"
-              placeholder="correo@crorr.com"
-              fullWidth
-              name="email"
-              value={email}
-              onChange={onInputChange}
-            />
-          </Grid>
-          <Grid item xs={12} sx={{ mt: 2 }}>
-            <TextField
-              label="Contraseña"
-              type="password"
-              placeholder="contraseña"
-              fullWidth
-              name="password"
-              value={password}
-              onChange={onInputChange}
-            />
-          </Grid>
-          <Grid container display={!!false ? '' : 'none'} sx={{ mt: 1 }}>
-            <Grid item xs={12}>
-              <Alert severity="error">error</Alert>
+        <form onSubmit={onSubmit}>
+          <Grid container>
+            <Grid item xs={12} sx={{ mt: 2 }}>
+              <TextField
+                label="Correo"
+                type="email"
+                placeholder="correo@crorr.com"
+                fullWidth
+                name="email"
+                value={email}
+                onChange={onInputChange}
+              />
+            </Grid>
+            <Grid item xs={12} sx={{ mt: 2 }}>
+              <TextField
+                label="Contraseña"
+                type="password"
+                placeholder="contraseña"
+                fullWidth
+                name="password"
+                value={password}
+                onChange={onInputChange}
+              />
+            </Grid>
+            <Grid container display={!!false ? '' : 'none'} sx={{ mt: 1 }}>
+              <Grid item xs={12}>
+                <Alert severity="error">error</Alert>
+              </Grid>
+            </Grid>
+            <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+              <Grid item xs={12} sm={6}>
+                <Button disabled={false} type="submit" variant="contained" fullWidth>
+                  Login
+                </Button>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Button disabled={true} variant="contained" fullWidth onClick={onGoogleSignIn}>
+                  <Google />
+                  <Typography sx={{ ml: 1 }}>Google</Typography>
+                </Button>
+              </Grid>
+            </Grid>
+            <Grid container direction="row" justifyContent="end">
+              <Link component={RouterLink} color="inherit" to="/auth/register">
+                Crear una cuenta
+              </Link>
             </Grid>
           </Grid>
-          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
-            <Grid item xs={12} sm={6}>
-              <Button disabled={false} type="submit" variant="contained" fullWidth>
-                Login
-              </Button>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Button disabled={true} variant="contained" fullWidth onClick={() => {}}>
-                <Google />
-                <Typography sx={{ ml: 1 }}>Google</Typography>
-              </Button>
-            </Grid>
-          </Grid>
-          <Grid container direction="row" justifyContent="end">
-            <Link component={RouterLink} color="inherit" to="/auth/register">
-              Crear una cuenta
-            </Link>
-          </Grid>
-        </Grid>
+        </form>
       </Grid>
     </Grid>
   )
