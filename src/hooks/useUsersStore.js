c15d6905ef3d25ff6dcd3aaeed5d1591b6dc4994
ariel <ariel.wm1@gmail.com>
2023-06-30 17:23:55 -0400

    update usersStore

--- src/hooks/useUsersStore.js
+++ src/hooks/useUsersStore.js
@@ -6,22 +6,24 @@ export const useUsersStore = () => {
   const { users } = useSelector(state => state.users)
   const dispatch = useDispatch()
 
+  // Obtener todos los usuarios
   const getAllUsers = async () => {
     try {
       const { data } = await schoolAdminApi.get('users')
-
       dispatch(loadUsers(data))
     } catch (error) {
       console.log(error)
     }
   }
 
+  // Crear un usuario
   const createUser = async user => {
     try {
       const { data } = await schoolAdminApi.post('users', user)
       dispatch(addUser(data))
     } catch (error) {
-      dispatch(loadError(error.response.data.message))
+      const errorMessage = error.response.data.message || 'An error occurred'
+      dispatch(loadError(errorMessage))
       console.log(error)
     }
   }
@@ -30,7 +32,6 @@ export const useUsersStore = () => {
     // propiedades
     users,
     // metodos
-
     getAllUsers,
     createUser,
   }
